d173342a5d0ebcb879d95cd5b68198d8d6975e7a
Jeff Terrell <jeffaterrell@gmail.com>
2021-10-25 16:09:34 -0400

    create files and folders, index.html content, and JS in scripts
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $("#favoritethings").submit(function(event) {
+  event.preventDefault();
+
+  
+  const foodInput = $("input#food").val();
+  const colorInput = $("input#color").val();
+  const animalInput = $("input#animal").val();
+  const hobbyInput = $("input#hobby").val();
+  const movieInput = $("input#movie").val();
+
+  // $(".food").text(foodInput);
+  // $(".color").text(colorInput);
+  // $(".animal").text(animalInput);
+  // $(".hobby").text(hobbyInput);
+  // $(".movie").text(movieInput);  
+
+
+  const favoriteThings = [foodInput, colorInput, animalInput, hobbyInput, movieInput];
+  // const test = favoriteThings.pop();
+  // const favoriteThings2 = favoriteThings.pop();
+
+  $(".showarray").append(favoriteThings);
+  $(".showarray").text(favoriteThings[1,2,3]);
+  
+  });
+});
